6e73fd16def6ebb417852fcb94bfefa4e7995266
Hugo Añamuro <hugo.aluque@gmail.com>
2024-06-13 12:50:03 -0500

    update- vistas

--- map.js
+++ map.js
@@ -175,19 +175,19 @@ L.control.layers(baseMaps, Capas, {
 
 //Adicionando Logo Institucional
 
-L.Control.Watermark=L.Control.extend({
-            onAdd:function(map){
-                var img = L.DomUtil.create('img');
-                img.src = 'img/PEBLT.png';
-                img.style.width = '150px';
-                return img;
-                },
-                onRemove:function(map){},
-                });
-                L.control.watermark = function(opts){
-                    return new L.Control.Watermark(opts);
-                    }
-                L.control.watermark({position:'bottomright'}).addTo(map);
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+        img.src = 'img/PEBLT.png';
+        img.style.width = '150px';
+        return img;
+    },
+    onRemove: function(map) {},
+});
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
+L.control.watermark({ position: 'topleft' }).addTo(map);
 // Color GRis 
 var baseMaps = {
     "<span style='color: gray'>Grayscale</span>": grayscale,
